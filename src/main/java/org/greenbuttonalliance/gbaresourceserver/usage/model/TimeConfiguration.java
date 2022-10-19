cd0cad79a127967641eeabb9aad30f11ecfbbe35
Carter_Webb <carterwebb2001@gmail.com>
2022-10-19 09:37:53 -0500

    Added copyright, removed unnecesary files, other changes

--- src/main/java/org/greenbuttonalliance/gbaresourceserver/usage/model/TimeConfiguration.java
+++ src/main/java/org/greenbuttonalliance/gbaresourceserver/usage/model/TimeConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2022 Green Button Alliance, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.greenbuttonalliance.gbaresourceserver.usage.model;
 
 import jakarta.persistence.*;
@@ -15,11 +30,11 @@ import lombok.experimental.SuperBuilder;
 @RequiredArgsConstructor
 public class TimeConfiguration extends IdentifiedObject{
 	//note: using the correct naming scheme for columns matters, auto generated code adn queries assume you follow the correct scheme
-	@Column
+	@Column(name= "dst_end_rule")
 	private byte[] dstEndRule;
-	@Column
+	@Column(name = "dst_offset")
 	private Long dstOffset;
-	@Column
+	@Column(name = "dst_start_rule")
 	private byte[] dstStartRule;
 	@Column
 	private Long tzOffset;
--- src/test/java/org/greenbuttonalliance/gbaresourceserver/usage/repository/TimeConfigurationRepositoryTest.java
+++ src/test/java/org/greenbuttonalliance/gbaresourceserver/usage/repository/TimeConfigurationRepositoryTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2022 Green Button Alliance, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.greenbuttonalliance.gbaresourceserver.usage.repository;
 
 import com.github.f4b6a3.uuid.UuidCreator;
@@ -21,7 +36,7 @@ import java.util.UUID;
 @DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class TimeConfigurationTest {
+public class TimeConfigurationRepositoryTest {
 	private final TimeConfigurationRepository timeConfigurationRepository;
 	private static final String SELF_LINK= "https://{domain}/espi/1_1/resource/RetailCustomer/9B6C7066/UsagePoint/5446AF3F/MeterReading/01/TimeConfiguration/183";
 	private static final String NOT_PRESENT_SELF_LINK = "foobar";
@@ -67,7 +82,7 @@ public class TimeConfigurationTest {
 			() -> String.format("findByAll size of %s does not match test data size of %s", findByAllSize, testDataSize)
 		);
 	}
-
+	//TODO: Add entityMappings_areNotNull test once Usage point is created
 
 
 	public static List<TimeConfiguration> buildTestData() {
