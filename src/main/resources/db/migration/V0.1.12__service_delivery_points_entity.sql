6063c2cbe9a2196e650b3dfc733ebdc58d73ecd2
SigurdFosite <72608761+SigurdFosite@users.noreply.github.com>
2022-10-30 22:51:00 -0500

    Created Datatable Entitiy scripts

---
+++ src/main/resources/db/migration/V0.1.12__service_delivery_points_entity.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS usage.service_delivery_points (
+                                                      uuid UUID PRIMARY KEY,
+                                                      customerAgreement TEXT,
+                                                      name TEXT,
+                                                      tariffProfile TEXT
+)
