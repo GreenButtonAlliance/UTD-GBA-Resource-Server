ea416cd902d3784d11091c7ea10ad87b5494e507
Suvanch <suvanch@gmail.com>
2022-11-05 22:53:38 -0500

    Formatting changes

--- src/test/java/org/greenbuttonalliance/gbaresourceserver/usage/web/controller/IntervalBlockControllerTest.java
+++ src/test/java/org/greenbuttonalliance/gbaresourceserver/usage/web/controller/IntervalBlockControllerTest.java
@@ -15,6 +15,13 @@
  */
 
 package org.greenbuttonalliance.gbaresourceserver.usage.web.controller;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+
+import java.io.File;
+import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,11 +32,75 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class IntervalBlockControllerTest {
 
+
+//Verifies if the number of interval blocks recived is the same as exxpected
+//To use test update IntervalBlock file the expectedValue
 	@Test
 	void getAll() {
+		int expectedValue = 20;
+		int actualValue = 0;
+
+
+		try {
+			//find and format the xml file, which is found in the same folder as this file and is named IntervalBlock
+			File file = new File("C:\\Users\\suvan\\Desktop\\seniorDesign\\GBA-Resource-Server\\src\\test\\java\\org\\greenbuttonalliance\\gbaresourceserver\\usage\\web\\controller\\IntervalBlock");
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+
+			//Counts the number of elements with the tag ID
+			actualValue = doc.getElementsByTagName("id").getLength();
+
+
+		}
+		catch (Exception e) {
+		e.printStackTrace();
+		}
+
+		//checks if the number of ID tags is the same as in the output file.
+		//Each interval block has one ID so counting for IDs will tell us the number of interval blocks
+		if(expectedValue==actualValue){
+			System.out.println("True");
+		}
+		else{
+			System.out.println("False");
+		}
 	}
 
+	//Check if a certin intervalblock exists in the XML output by checking for its UUID
+	//To use test update IntervalBlock file the IntervalBlock
 	@Test
-	void getByUuid() {
+	void getByUuid() throws IOException {
+		String expectedUUID = "65866e0d-4a9b-555b-8d38-ef2d9d51fc11";
+		Boolean foundUUID = false;
+
+		try {
+			//find and format the xml file, which is found in the same folder as this file and is named IntervalBlock
+			File file = new File("C:\\Users\\suvan\\Desktop\\seniorDesign\\GBA-Resource-Server\\src\\test\\java\\org\\greenbuttonalliance\\gbaresourceserver\\usage\\web\\controller\\IntervalBlock");
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+
+			//Counts the number of elements with the tag ID
+			int length  = doc.getElementsByTagName("id").getLength();
+
+			//Loops though them to find if one of recived UUIDS is the same as the expectedUUID
+			for(int i=0;i<length-1;i++){
+				String UUID1 = doc.getElementsByTagName("id").item(i).getTextContent();
+				if(UUID1.contains(expectedUUID)){
+					foundUUID = true;
+				}
+			}
+
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(foundUUID);
 	}
+
 }
