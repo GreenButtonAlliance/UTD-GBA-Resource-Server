298ef0e2f676dba111ab187f25229831c3cf0d40
SigurdFosite <72608761+SigurdFosite@users.noreply.github.com>
2022-11-09 21:40:48 -0600

    Added Enums

--- src/main/resources/db/migration/V0.1.12__service_delivery_points_entity.sql
+++ src/main/resources/db/migration/V0.1.12__service_delivery_points_entity.sql
@@ -1,3 +1,18 @@
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_type t INNER JOIN pg_namespace ns ON t.typnamespace = ns.oid WHERE t.typname = 'time_period_of_interest' AND ns.nspname = 'usage') THEN
+      CREATE TYPE usage.time_period_of_interest AS ENUM (
+        'NONE',
+        'BILLING_PERIOD',
+        'DAILY',
+        'MONTHLY',
+        'SEASONAL',
+        'WEEKLY',
+        'SPECIFIED_PERIOD'
+        );
+    END IF;
+  END $$;
+
 CREATE TABLE IF NOT EXISTS usage.service_delivery_points (
                                                       uuid UUID PRIMARY KEY,
                                                       customerAgreement TEXT,
