9a4eedea8b28d95c53111624645ab4ee3d52b326
SigurdFosite <72608761+SigurdFosite@users.noreply.github.com>
2022-12-07 22:14:29 -0600

    Merged enums into one sql file

--- src/main/resources/db/migration/V0.1/V0.1.13__usage_points_entity.sql
+++ src/main/resources/db/migration/V0.1/V0.1.13__usage_points_entity.sql
@@ -1,3 +1,89 @@
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_type t INNER JOIN pg_namespace ns ON t.typnamespace = ns.oid WHERE t.typname = 'service_kind' AND ns.nspname = 'usage') THEN
+      CREATE TYPE usage.service_kind AS ENUM (
+        'ELECTRICITY',
+        'GAS',
+        'WATER',
+        'TIME',
+        'HEAT',
+        'REFUSE',
+        'SEWERAGE',
+        'RATES',
+        'TVLICENSE',
+        'INTERNET',
+        'WEATHER'
+        );
+    END IF;
+  END $$;
+
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_type t INNER JOIN pg_namespace ns ON t.typnamespace = ns.oid WHERE t.typname = 'am_i_billing_ready_kind' AND ns.nspname = 'usage') THEN
+      CREATE TYPE usage.am_i_billing_ready_kind AS ENUM (
+        'AMICAPABLE',
+        'AMIDISABLED',
+        'BILLINGAPPROVED',
+        'ENABLED',
+        'NONAMI',
+        'NONMETERED',
+        'OPERABLE'
+        );
+    END IF;
+  END $$;
+
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_type t INNER JOIN pg_namespace ns ON t.typnamespace = ns.oid WHERE t.typname = 'usage_point_connected_kind' AND ns.nspname = 'usage') THEN
+      CREATE TYPE usage.usage_point_connected_kind AS ENUM (
+        'CONNECTED',
+        'LOGICALLYDISCONNECTED',
+        'PHYSICALLYDISCONNECTED'
+        );
+    END IF;
+  END $$;
+
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_type t INNER JOIN pg_namespace ns ON t.typnamespace = ns.oid WHERE t.typname = 'apnode_type' AND ns.nspname = 'usage') THEN
+      CREATE TYPE usage.apnode_type AS ENUM (
+        'AG',
+        'CPZ',
+        'DPZ',
+        'LAP',
+        'TH',
+        'SYS',
+        'CA',
+        'DCA',
+        'GA',
+        'GH',
+        'EHV',
+        'ZN',
+        'INT',
+        'BUS'
+        );
+    END IF;
+  END $$;
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_type t INNER JOIN pg_namespace ns ON t.typnamespace = ns.oid WHERE t.typname = 'anode_type' AND ns.nspname = 'usage') THEN
+      CREATE TYPE usage.anode_type AS ENUM (
+        'SYS',
+        'RUC',
+        'LFZ',
+        'REG',
+        'AGR',
+        'POD',
+        'ALR',
+        'LTAC',
+        'ACA',
+        'ASR',
+        'ECA'
+        );
+    END IF;
+  END $$;
+
+
 CREATE TABLE IF NOT EXISTS usage.service_delivery_points (
                                                            uuid UUID PRIMARY KEY,
                                                            customerAgreement TEXT,
