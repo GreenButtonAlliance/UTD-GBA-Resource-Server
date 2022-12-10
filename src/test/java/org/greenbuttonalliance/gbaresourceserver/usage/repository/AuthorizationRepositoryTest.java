f01ca18c0d67b3a779e686f4dcde8dcd1ebe6768
SigurdFosite <72608761+SigurdFosite@users.noreply.github.com>
2022-12-09 20:45:50 -0600

    Completed AuthorizationRepositoryTest with subscription connection added

--- src/test/java/org/greenbuttonalliance/gbaresourceserver/usage/repository/AuthorizationRepositoryTest.java
+++ src/test/java/org/greenbuttonalliance/gbaresourceserver/usage/repository/AuthorizationRepositoryTest.java
@@ -18,10 +18,15 @@ package org.greenbuttonalliance.gbaresourceserver.usage.repository;
 
 import com.github.f4b6a3.uuid.UuidCreator;
 import lombok.RequiredArgsConstructor;
+import org.greenbuttonalliance.gbaresourceserver.customer.model.CustomerAccount;
+import org.greenbuttonalliance.gbaresourceserver.customer.model.Organisation;
+import org.greenbuttonalliance.gbaresourceserver.usage.model.ApplicationInformation;
 import org.greenbuttonalliance.gbaresourceserver.usage.model.Authorization;
+import org.greenbuttonalliance.gbaresourceserver.usage.model.RetailCustomer;
 import org.greenbuttonalliance.gbaresourceserver.usage.model.Subscription;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.OAuthError;
+import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +37,8 @@ import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 @DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -87,7 +94,17 @@ public class AuthorizationRepositoryTest {
 
 	@Test
 	public void entityMappings_areNotNull() {
+		Authorization fullyMappedAuthorization = authorizationRepository.findById(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, PRESENT_SELF_LINK)).orElse(null);
+		Assumptions.assumeTrue(fullyMappedAuthorization != null);
 
+		Function<Authorization, Optional<Subscription>> authorizationSubscriptionInfo = auth -> Optional.ofNullable(auth.getSubscription());
+
+		Assertions.assertAll(
+			"Entity mapping failures for customer account " + fullyMappedAuthorization.getUuid(),
+			Stream.of(authorizationSubscriptionInfo)
+				.map(mappingFunc ->
+					() -> Assertions.assertTrue(mappingFunc.apply(fullyMappedAuthorization).isPresent()))
+		);
 	}
 
 	private static List<Authorization> buildTestData() {
@@ -122,14 +139,79 @@ public class AuthorizationRepositoryTest {
 				.tokenType(54)
 				.applicationInformationId(UUID.randomUUID())
 				.retailCustomerId(UUID.randomUUID())
-				.subscription(Subscription.builder().build())
+				.subscription(Subscription.builder().selfLinkHref("asdfasdf").build())
 				.build()
 		);
 
 		// hydrate UUIDs and entity mappings
-		authorizations.forEach(ai -> {
-			ai.setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, ai.getSelfLinkHref()));
+		authorizations.forEach(auth -> {
+			auth.setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, auth.getSelfLinkHref()));
 
+			Subscription sub = auth.getSubscription();
+			sub.setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, auth.getSelfLinkHref()));
+			sub.setAuthorization(auth);
+
+			ApplicationInformation ai = ApplicationInformation.builder()
+				.description(DUMMY_STRING)
+				.published(LocalDateTime.parse("2014-01-02 05:00:00", SQL_FORMATTER))
+				.selfLinkHref("https://{domain}/DataCustodian/espi/1_1/resource/ApplicationInformation/2")
+				.selfLinkRel("self")
+				.upLinkHref("https://{domain}/DataCustodian/espi/1_1/resource/ApplicationInformation")
+				.upLinkRel("up")
+				.updated(LocalDateTime.parse("2014-01-02 05:00:00", SQL_FORMATTER))
+				.authorizationServerAuthorizationEndpoint(DUMMY_STRING)
+				.authorizationServerRegistrationEndpoint(null)
+				.authorizationServerTokenEndpoint(DUMMY_STRING)
+				.authorizationServerUri(null)
+				.clientId(DUMMY_STRING)
+				.clientIdIssuedAt(1403190000L)
+				.clientName(DUMMY_STRING)
+				.clientSecret(DUMMY_STRING)
+				.clientSecretExpiresAt(0L)
+				.clientUri(null)
+				.contacts(new HashSet<>(Arrays.asList(
+					"Contact1",
+					"Contact3"
+				)))
+				.dataCustodianApplicationStatus(DataCustodianApplicationStatus.ON_HOLD)
+				.dataCustodianBulkRequestUri(DUMMY_STRING)
+				.dataCustodianId(DUMMY_STRING)
+				.dataCustodianResourceEndpoint(DUMMY_STRING)
+				.thirdPartyScopeSelectionScreenUri(null)
+				.thirdPartyUserPortalScreenUri(null)
+				.logoUri(null)
+				.policyUri(null)
+				.thirdPartyApplicationDescription(null)
+				.thirdPartyApplicationStatus(ThirdPartyApplicationStatus.PRODUCTION)
+				.thirdPartyApplicationType(ThirdPartyApplicationType.DEVICE)
+				.thirdPartyApplicationUse(ThirdPartyApplicationUse.COMPARISONS)
+				.thirdPartyPhone(null)
+				.thirdPartyNotifyUri(DUMMY_STRING)
+				.redirectUris(new HashSet<>(Arrays.asList(
+					"Redirect2",
+					"Redirect3"
+				)))
+				.tosUri(null)
+				.softwareId(DUMMY_STRING)
+				.softwareVersion(DUMMY_STRING)
+				.tokenEndpointAuthMethod(TokenEndpointMethod.BASIC)
+				.responseType(ResponseType.CODE)
+				.registrationClientUri(DUMMY_STRING)
+				.registrationAccessToken(DUMMY_STRING)
+				.thirdPartyScopeSelectionScreenUri(null)
+				.dataCustodianScopeSelectionScreenUri(null)
+				.grantTypes(new HashSet<>(Arrays.asList(
+					GrantType.AUTHORIZATION_CODE
+				)))
+				.scopes(new HashSet<>(Arrays.asList(
+					"Scope4"
+				)))
+				.build();
+			ai.setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, auth.getSelfLinkHref()));
+			sub.setApplicationInformation(ai);
+
+			sub.setRetailCustomer(RetailCustomer.builder().build());
+			sub.getRetailCustomer().setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, sub.getSelfLinkHref()));
 			// TODO hydrate UsagePoint reference once entity is available
 		});
 		return authorizations;
