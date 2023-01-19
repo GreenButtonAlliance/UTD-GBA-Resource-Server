9d0677d5bba64bb648f5323e26eb1c22549e2579
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-01-19 00:20:17 -0500

    Refactor flyway V0.1.15_TariffRiderRef_Refactor and V0.1.17_subscription_entity

--- src/main/resources/db/migration/V0.1/V0.1.17__subscription_entity.sql
+++ src/main/resources/db/migration/V0.1/V0.1.17__subscription_entity.sql
@@ -14,4 +14,11 @@ CREATE TABLE IF NOT EXISTS usage.subscription (
                                                 retail_customer_id UUID REFERENCES usage.retail_customer,
                                                 usage_point_id UUID --TODO
 );
-ALTER TABLE usage.authorization ADD CONSTRAINT fk_subscription FOREIGN KEY (subscription_id) REFERENCES usage.subscription;
+
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_constraint WHERE conname = 'fk_subscription') THEN
+      ALTER TABLE usage.authorization ADD CONSTRAINT fk_subscription FOREIGN KEY (subscription_id) REFERENCES
+        usage.subscription;
+    END IF;
+  END $$;
