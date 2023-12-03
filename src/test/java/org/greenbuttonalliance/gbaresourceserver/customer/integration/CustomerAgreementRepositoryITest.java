c2b38465e23397b336db0e5b9d94c9cdd559ec11
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-12-02 23:02:45 -0500

    Refactor CustomerAgreementRepositoryITest to use Testcontainers #108
    Signed-off-by: Donald F. Coffin <dcoffin@greenbuttonalliance.org>

--- src/test/java/org/greenbuttonalliance/gbaresourceserver/customer/integration/CustomerAgreementRepositoryITest.java
+++ src/test/java/org/greenbuttonalliance/gbaresourceserver/customer/integration/CustomerAgreementRepositoryITest.java
@@ -35,6 +35,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Arrays;
 import java.util.List;
@@ -45,6 +49,9 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Testcontainers
 @DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CustomerAgreementRepositoryITest {
@@ -55,10 +62,20 @@ public class CustomerAgreementRepositoryITest {
 	private static final String PRESENT_AGREEMENT_ID = "foo";
 	private static final String NOT_PRESENT_AGREEMENT_ID = "bar";
 
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
 	@BeforeEach
 	public void initTestData() {
 		customerAgreementRepository.deleteAllInBatch();
 		customerAgreementRepository.saveAll(buildTestData());
+	}
+
+	@Test
+	void connectionEstablished() {
+		assertThat(postgres.isCreated()).isTrue();
+		assertThat(postgres.isRunning()).isTrue();
 	}
 
 	@Test
