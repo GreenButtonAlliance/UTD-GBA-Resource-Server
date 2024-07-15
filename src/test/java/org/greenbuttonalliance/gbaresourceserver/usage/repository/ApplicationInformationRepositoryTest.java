bbefdec1ba9f23ba542fa63bebe98ba615b4f896
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2024-07-15 18:17:04 -0400

    Polish remove unneccessary test for query by clientId and dataCustodianId #113
    Signed-off-by: Donald F. Coffin <dcoffin@greenbuttonalliance.org>

--- src/test/java/org/greenbuttonalliance/gbaresourceserver/usage/repository/ApplicationInformationRepositoryTest.java
+++ src/test/java/org/greenbuttonalliance/gbaresourceserver/usage/repository/ApplicationInformationRepositoryTest.java
@@ -17,23 +17,11 @@ package org.greenbuttonalliance.gbaresourceserver.usage.repository;
 
 import com.github.f4b6a3.uuid.UuidCreator;
 import lombok.RequiredArgsConstructor;
-import org.greenbuttonalliance.gbaresourceserver.common.model.DateTimeInterval;
+import org.greenbuttonalliance.gbaresourceserver.TestDataBuilder;
 import org.greenbuttonalliance.gbaresourceserver.usage.model.ApplicationInformation;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.Authorization;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.RetailCustomer;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.Subscription;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.AuthorizationStatus;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.DataCustodianApplicationStatus;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.GrantType;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.ResponseType;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.ThirdPartyApplicationStatus;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.ThirdPartyApplicationType;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.ThirdPartyApplicationUse;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.TokenEndpointMethod;
-import org.greenbuttonalliance.gbaresourceserver.usage.model.enums.TokenType;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -44,14 +32,8 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -62,291 +44,71 @@ import static org.assertj.core.api.Assertions.*;
 public class ApplicationInformationRepositoryTest {
 	private final ApplicationInformationRepository applicationInformationRepository;
 
-	private static final String PRESENT_SELF_LINK = "https://localhost:8080/DataCustodian/espi/1_1/resource" +
-			"/ApplicationInformation/135497";
-	private static final String UP_LINK = "https://localhost:8080/DataCustodian/espi/1_1/resource" +
-			"/ApplicationInformation";
-	private static final String NOT_PRESENT_SELF_LINK = "foobar";
-	private static final String DUMMY_STRING = "test1";
-
 	@Container
 	@ServiceConnection
 	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
 	@BeforeEach
-	public void initTestData() {
+	public void setup() {
 		applicationInformationRepository.deleteAllInBatch();
-		applicationInformationRepository.saveAll(buildTestData());
+
+		//Generate two ApplicationInformation objects
+		var appInfo1 = TestDataBuilder.buildTestApplicationInformation(
+			"https://localhost:8080/DataCustodian/espi/1_1/resource/ApplicationInformation/135497",
+			"GBA",
+			"GBA Electricity & Gas");
+		var appInfo2 = TestDataBuilder.buildTestApplicationInformation(
+			"https://localhost:8080/DataCustodian/espi/1_1/resource/ApplicationInformation/284915",
+			"UtilityAPI",
+			"Bluewater Power Distribution Corporation");
+
+		//Save both ApplicationInformation objects to the repository
+		applicationInformationRepository.saveAll(Arrays.asList(appInfo1, appInfo2));
 	}
 
 	@Test
+	@DisplayName("Connection to Postgres container is established")
 	void connectionEstablished() {
 		assertThat(postgres.isCreated()).isTrue();
 		assertThat(postgres.isRunning()).isTrue();
 	}
 
 	@Test
-	public void findByPresentId_returnsMatching() {
-		UUID presentUuid = UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, PRESENT_SELF_LINK);
-		UUID foundUuid = applicationInformationRepository.findById(presentUuid).map(ApplicationInformation::getUuid).orElse(null);
+	@DisplayName("ApplicationInformation Id present returns matching object")
+	public void findByUuid_returnsCorrectApplicationInformation() {
+		// Use the specific self-link HREFs to find the UUIDs
+		UUID presentUuid1 = UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL,
+			"https://localhost:8080/DataCustodian/espi/1_1/resource/ApplicationInformation/135497");
+		UUID foundUuid1 = applicationInformationRepository.findById(presentUuid1).map(ApplicationInformation::getUuid).orElse(null);
+		Assertions.assertEquals(presentUuid1, foundUuid1);
 
-		Assertions.assertEquals(
-			presentUuid,
-			foundUuid,
-			() -> String.format("findById with %s returns entity with ID %s", presentUuid, foundUuid)
-		);
+		// Repeat for the second ApplicationInformation object
+		UUID presentUuid2 = UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL,
+			"https://localhost:8080/DataCustodian/espi/1_1/resource/ApplicationInformation/284915");
+		UUID foundUuid2 = applicationInformationRepository.findById(presentUuid2).map(ApplicationInformation::getUuid).orElse(null);
+		Assertions.assertEquals(presentUuid2, foundUuid2);
 	}
 
 	@Test
-	public void findByNotPresentId_returnsEmpty() {
-		UUID notPresentUuid = UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, NOT_PRESENT_SELF_LINK);
-		Optional<ApplicationInformation> applicationInformation = applicationInformationRepository.findById(notPresentUuid);
+	@DisplayName("ApplicationInformation Id non-existing returns empty Optional")
+	void findByNotPresentId_returnsEmpty() {
+		// Generate a UUID that is guaranteed not to exist in the database
+		UUID nonExistingUuid = UUID.randomUUID();
 
-		Assertions.assertTrue(
-			applicationInformation.isEmpty(),
-			() -> String.format("findById with %s returns entity with ID %s", notPresentUuid, applicationInformation.map(ApplicationInformation::getUuid).orElse(null))
-		);
+		// Attempt to find an ApplicationInformation by the non-existing UUID
+		Optional<ApplicationInformation> result = applicationInformationRepository.findById(nonExistingUuid);
+
+		// Assert that the result is empty
+		Assertions.assertTrue(result.isEmpty(), "Expected an empty Optional when querying a non-existing UUID");
 	}
 
 	@Test
+	@DisplayName("ApplicationInformation request with no Id returns all objects")
 	public void findAll_returnsAll() {
 		int findByAllSize = applicationInformationRepository.findAll().size();
-		int testDataSize = buildTestData().size();
+		// Directly compare with the known number of ApplicationInformation objects added
+		int expectedSize = 2; // Since we added two ApplicationInformation objects in setup
 
-		Assertions.assertEquals(
-			findByAllSize,
-			testDataSize,
-			() -> String.format("findByAll size of %s does not match test data size of %s", findByAllSize, testDataSize)
-		);
-	}
-
-	@Test
-	public void entityMappings_areNotNull() {
-		ApplicationInformation fullyMappedApplicationInformation = applicationInformationRepository.findById(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, PRESENT_SELF_LINK)).orElse(null);
-		Assumptions.assumeTrue(fullyMappedApplicationInformation != null);
-
-		Function<ApplicationInformation, Optional<Set<Subscription>>> applicationInformationToSubscription = ai -> Optional.ofNullable(ai.getSubscriptions());
-		// TODO test mapping to UsagePoint once entity is available
-
-		Assertions.assertAll(
-			"Entity mapping failures for applicationInformation " + fullyMappedApplicationInformation.getUuid(),
-			Stream.of(applicationInformationToSubscription)
-				.map(mappingFunc ->
-					() -> Assertions.assertTrue(mappingFunc.apply(fullyMappedApplicationInformation).isPresent()))
-		);
-	}
-
-
-	private static List<ApplicationInformation> buildTestData() {
-		List<ApplicationInformation> applicationInformations = Arrays.asList(
-			ApplicationInformation.builder()
-				.description("Green Button Alliance Data Custodian Admin Application")
-				.selfLinkHref(PRESENT_SELF_LINK)
-				.upLinkHref("UP_LINK")
-				.authorizationServerAuthorizationEndpoint(DUMMY_STRING)
-				.authorizationServerRegistrationEndpoint(null)
-				.authorizationServerTokenEndpoint(DUMMY_STRING)
-				.authorizationServerUri(null)
-				.clientId(DUMMY_STRING)
-				.clientIdIssuedAt(1403190000L)
-				.clientName(DUMMY_STRING)
-				.clientSecret(DUMMY_STRING)
-				.clientSecretExpiresAt(0L)
-				.clientUri(null)
-				.contacts(new HashSet<>(Arrays.asList(
-					"Contact1",
-					"Contact2"
-				)))
-				.dataCustodianApplicationStatus(DataCustodianApplicationStatus.ON_HOLD)
-				.dataCustodianBulkRequestUri(DUMMY_STRING)
-				.dataCustodianId(DUMMY_STRING)
-				.dataCustodianResourceEndpoint(DUMMY_STRING)
-				.thirdPartyUserPortalScreenUri(null)
-				.logoUri(null)
-				.policyUri(null)
-				.thirdPartyApplicationDescription(null)
-				.thirdPartyApplicationStatus(ThirdPartyApplicationStatus.PRODUCTION)
-				.thirdPartyApplicationType(ThirdPartyApplicationType.DEVICE)
-				.thirdPartyApplicationUse(ThirdPartyApplicationUse.COMPARISONS)
-				.thirdPartyPhone(null)
-				.thirdPartyNotifyUri(DUMMY_STRING)
-				.redirectUris(new HashSet<>(List.of(
-						"Redirect1"
-				)))
-				.tosUri(null)
-				.softwareId(DUMMY_STRING)
-				.softwareVersion(DUMMY_STRING)
-				.tokenEndpointAuthMethod(TokenEndpointMethod.BASIC)
-				.responseType(ResponseType.CODE)
-				.registrationClientUri(DUMMY_STRING)
-				.registrationAccessToken(DUMMY_STRING)
-				.grantTypes(new HashSet<>(List.of(
-						GrantType.AUTHORIZATION_CODE
-				)))
-				.scopes(new HashSet<>(Arrays.asList(
-					"Scope1",
-					"Scope2",
-					"Scope3"
-				)))
-				.subscriptions(new HashSet<>(
-					Collections.singletonList(
-						Subscription.builder()
-							.description("description")
-							.selfLinkHref("https://localhost:8080/espi/1_1/resource/Subscription/100001")
-							.upLinkHref("https://localhost:8080/DataCustodian/espi/1_1/resource/Subscription")
-							.hashedId("hashedId")
-							.authorization(
-								Authorization.builder()
-									.uuid(UUID.randomUUID())
-									.description("Green Button Alliance Data Custodian Authorization")
-									.selfLinkHref(PRESENT_SELF_LINK)
-									.upLinkHref("https://localhost:8080/DataCustodian/espi/1_1/resource" +
-											"/Authorization")
-										.authorizedPeriod(new DateTimeInterval()
-												.setDuration(21L)
-												.setStart(654L)
-												.setDuration(23123L)
-												.setStart(3241654L))
-										.status(AuthorizationStatus.ACTIVE)
-										.expiresAt(32164L)
-										.scope("scope")
-										.tokenType(TokenType.BEARER)
-										.resourceUri("resourceUri")
-										.authorizationUri("authorizationUri")
-										.customerResourceUri("customerResourceUri")
-//									.applicationInformationId(UUID.randomUUID())
-//									.retailCustomerId(UUID.randomUUID())
-//									.subscription(Subscription.builder().build())
-									.build()
-							)
-							.retailCustomer(RetailCustomer.builder()
-								.description("Type of Meter Reading Data")
-								.selfLinkHref(PRESENT_SELF_LINK)
-								.upLinkHref("https://localhost:8080/espi/1_1/resource/RetailCustomer")
-								.enabled(Boolean.TRUE)
-								.firstName("hello")
-								.lastName("last")
-								.password("password")
-								.role("whatever")
-								.username("Username")
-								.build())
-//				.usagePointId(1)
-							.build()
-					))
-				)
-				.build(),
-			ApplicationInformation.builder()
-				.description(DUMMY_STRING)
-				.selfLinkHref("https://localhost:8080/DataCustodian/espi/1_1/resource/ApplicationInformation/284915")
-				.upLinkHref(UP_LINK)
-				.authorizationServerAuthorizationEndpoint(DUMMY_STRING)
-				.authorizationServerRegistrationEndpoint(null)
-				.authorizationServerTokenEndpoint(DUMMY_STRING)
-				.authorizationServerUri(null)
-				.clientId(DUMMY_STRING)
-				.clientIdIssuedAt(1403190000L)
-				.clientName(DUMMY_STRING)
-				.clientSecret(DUMMY_STRING)
-				.clientSecretExpiresAt(0L)
-				.clientUri(null)
-				.contacts(new HashSet<>(Arrays.asList(
-					"Contact1",
-					"Contact3"
-				)))
-				.dataCustodianApplicationStatus(DataCustodianApplicationStatus.ON_HOLD)
-				.dataCustodianBulkRequestUri(DUMMY_STRING)
-				.dataCustodianId(DUMMY_STRING)
-				.dataCustodianResourceEndpoint(DUMMY_STRING)
-				.thirdPartyUserPortalScreenUri(null)
-				.logoUri(null)
-				.policyUri(null)
-				.thirdPartyApplicationDescription(null)
-				.thirdPartyApplicationStatus(ThirdPartyApplicationStatus.PRODUCTION)
-				.thirdPartyApplicationType(ThirdPartyApplicationType.DEVICE)
-				.thirdPartyApplicationUse(ThirdPartyApplicationUse.COMPARISONS)
-				.thirdPartyPhone(null)
-				.thirdPartyNotifyUri(DUMMY_STRING)
-				.redirectUris(new HashSet<>(Arrays.asList(
-					"Redirect2",
-					"Redirect3"
-				)))
-				.tosUri(null)
-				.softwareId(DUMMY_STRING)
-				.softwareVersion(DUMMY_STRING)
-				.tokenEndpointAuthMethod(TokenEndpointMethod.BASIC)
-				.responseType(ResponseType.CODE)
-				.registrationClientUri(DUMMY_STRING)
-				.registrationAccessToken(DUMMY_STRING)
-				.grantTypes(new HashSet<>(List.of(
-						GrantType.AUTHORIZATION_CODE
-				)))
-				.scopes(new HashSet<>(List.of(
-						"Scope4"
-				)))
-				.subscriptions(new HashSet<>(
-						Collections.singletonList(
-							Subscription.builder()
-								.description("description")
-								.selfLinkHref("https://localhost:8080/espi/1_1/resource/Subscription/176")
-								.upLinkHref("https://localhost:8080/DataCustodian/espi/1_1/resource/Subscription")
-								.hashedId("hashedId")
-								.authorization(
-									Authorization.builder()
-										.uuid(UUID.randomUUID())
-										.description("Green Button Alliance Data Custodian Authorization")
-										.selfLinkHref(PRESENT_SELF_LINK)
-										.upLinkHref("https://localhost:8080/DataCustodian/espi/1_1/resource" +
-												"/Authorization")
-										.authorizedPeriod(new DateTimeInterval()
-											.setDuration(21L)
-											.setStart(654L)
-											.setDuration(23123L)
-											.setStart(3241654L))
-										.status(AuthorizationStatus.ACTIVE)
-										.expiresAt(32164L)
-										.scope("scope")
-										.tokenType(TokenType.BEARER)
-										.resourceUri("resourceUri")
-										.authorizationUri("authorizationUri")
-										.customerResourceUri("customerResourceUri")
-//										.applicationInformationId(UUID.randomUUID())
-//										.retailCustomerId(UUID.randomUUID())
-//										.subscription(Subscription.builder().build())
-										.build()
-								)
-								.retailCustomer(RetailCustomer.builder()
-									.description("Type of Meter Reading Data")
-									.selfLinkHref(PRESENT_SELF_LINK)
-									.upLinkHref("https://localhost:8080/espi/1_1/resource/RetailCustomer")
-									.enabled(Boolean.TRUE)
-									.firstName("hello")
-									.lastName("last")
-									.password("password")
-									.role("whatever")
-									.username("Username")
-									.build())
-//				.usagePointId(1)
-								.build()
-						))
-				)
-				.build()
-		);
-
-		// hydrate UUIDs and entity mappings
-		applicationInformations.forEach(ai -> {
-			ai.setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, ai.getSelfLinkHref()));
-
-			ai.getSubscriptions().forEach(sub -> {
-				sub.setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, sub.getSelfLinkHref()));
-				sub.setApplicationInformation(ai);
-				sub.setAuthorization(Authorization.builder().build());
-				sub.getAuthorization().setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, sub.getSelfLinkHref()));
-				sub.setRetailCustomer(RetailCustomer.builder().build());
-				sub.getRetailCustomer().setUuid(UuidCreator.getNameBasedSha1(UuidCreator.NAMESPACE_URL, sub.getSelfLinkHref()));
-			});
-
-			// TODO hydrate UsagePoint reference once entity is available
-		});
-		return applicationInformations;
+		Assertions.assertEquals(expectedSize, findByAllSize);
 	}
 }
